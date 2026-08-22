bd6a1042d683894694dd17082515dda8c7aba6e1
unknown <riaz.com2025@gmail.com>
2026-08-22 20:13:35 +0600

    Add Bootstrap styling for professional UI

--- students/forms.py
+++ students/forms.py
@@ -7,3 +7,18 @@ class StudentForm(forms.ModelForm):
         fields = ['form_no', 'student_id', 'name', 'admission_class', 'section',
                   'admission_year', 'roll_no', 'gender', 'religion', 'father_name',
                   'contact_no', 'guardian_contact_no', 'group']
+        widgets = {
+            'form_no': forms.TextInput(attrs={'class': 'form-control'}),
+            'student_id': forms.TextInput(attrs={'class': 'form-control'}),
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'admission_class': forms.TextInput(attrs={'class': 'form-control'}),
+            'section': forms.TextInput(attrs={'class': 'form-control'}),
+            'admission_year': forms.NumberInput(attrs={'class': 'form-control'}),
+            'roll_no': forms.NumberInput(attrs={'class': 'form-control'}),
+            'gender': forms.Select(attrs={'class': 'form-select'}),
+            'religion': forms.TextInput(attrs={'class': 'form-control'}),
+            'father_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'contact_no': forms.TextInput(attrs={'class': 'form-control'}),
+            'guardian_contact_no': forms.TextInput(attrs={'class': 'form-control'}),
+            'group': forms.Select(attrs={'class': 'form-select'}),
+        }
