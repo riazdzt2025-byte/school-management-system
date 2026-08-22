4b28b2b6e89e99c165c506b06bef82deaec2bbbb
unknown <riaz.com2025@gmail.com>
2026-08-22 19:46:23 +0600

    Add edit and delete student features - full CRUD complete

--- students/views.py
+++ students/views.py
@@ -1,19 +1,12 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
-from .models import Student
-
-def student_list(request):
-    students = Student.objects.all()
-    return render(request, 'students/student_list.html', {'students': students})
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Student
 from .forms import StudentForm
 
+
 def student_list(request):
     students = Student.objects.all()
     return render(request, 'students/student_list.html', {'students': students})
+
 
 def add_student(request):
     if request.method == 'POST':
@@ -24,3 +17,23 @@ def add_student(request):
     else:
         form = StudentForm()
     return render(request, 'students/add_student.html', {'form': form})
+
+
+def edit_student(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    if request.method == 'POST':
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('student_list')
+    else:
+        form = StudentForm(instance=student)
+    return render(request, 'students/add_student.html', {'form': form})
+
+
+def delete_student(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    if request.method == 'POST':
+        student.delete()
+        return redirect('student_list')
+    return render(request, 'students/delete_student.html', {'student': student})
